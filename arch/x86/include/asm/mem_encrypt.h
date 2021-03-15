ef009c0f1326bb6d25397812a05326733886fe77
Weidong Cui <weidongcui@gmail.com>
2021-03-15 16:51:10 -0700

    Support #HV in SEV-SNP

--- arch/x86/include/asm/mem_encrypt.h
+++ arch/x86/include/asm/mem_encrypt.h
@@ -51,6 +51,7 @@ void __init mem_encrypt_free_decrypted_mem(void);
 void __init mem_encrypt_init(void);
 
 void __init sev_es_init_vc_handling(void);
+void __init sev_snp_init_hv_handling(void);
 bool sme_active(void);
 bool sev_active(void);
 bool sev_es_active(void);
@@ -81,6 +82,7 @@ static inline void __init sme_encrypt_kernel(struct boot_params *bp) { }
 static inline void __init sme_enable(struct boot_params *bp) { }
 
 static inline void sev_es_init_vc_handling(void) { }
+static inline void sev_snp_init_hv_handling(void) { }
 static inline bool sme_active(void) { return false; }
 static inline bool sev_active(void) { return false; }
 static inline bool sev_es_active(void) { return false; }
